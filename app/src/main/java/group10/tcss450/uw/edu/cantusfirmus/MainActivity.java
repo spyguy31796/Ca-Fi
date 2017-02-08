47bf9f72063460fb80ab32ec199fc389265dd85a
spyguy31796 <planetalec@live.com>
2017-02-08 00:45:12 -0800

    Added Permission Check for Marshmallow

--- app/src/main/java/group10/tcss450/uw/edu/cantusfirmus/MainActivity.java
+++ app/src/main/java/group10/tcss450/uw/edu/cantusfirmus/MainActivity.java
@@ -1,20 +1,24 @@
 package group10.tcss450.uw.edu.cantusfirmus;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int MY_PERMISSIONS_REQUEST_READ_STORAGE = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        requestPermissions();
         Button playMusicbtn = (Button) findViewById(R.id.MusicPlayerBtn);
-
         playMusicbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -23,7 +27,26 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
+    public void requestPermissions(){
+        //Requests permissions
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_STORAGE);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_STORAGE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //Storage access is available
+                }else{
+                    //Permission Denied
+                    Toast.makeText(MainActivity.this,"Storage Permission Denied, Playback Disabled",Toast.LENGTH_LONG);
+                }
+                return;
+            }
+        }
+    }
     private void playMusic(View view) {
         Intent intent = new Intent(this, audio_player.class);
         startActivity(intent);
